f4d0f942c2b68cae56ed367251a5d4499a8f2a8a
Houzair Koussa <houzairmk@icloud.com>
2022-09-10 14:03:53 +0400

    [ADD] mondo creation func

--- routes/dev.js
+++ routes/dev.js
@@ -7,7 +7,7 @@ routes.route('/devs').get(async (_req, res) => {
   const dbConnect = dbo.getDb();
 
   dbConnect
-    .collection(' ') // !
+    .collection('devs')
     .find({})
     .limit(50)
     .toArray((err, result) => {
@@ -15,6 +15,32 @@ routes.route('/devs').get(async (_req, res) => {
         res.status(400).send({ message: err.message });
       } else {
         res.json(result);
+      }
+    });
+});
+
+routes.route('/devs').post((req, res) => {
+  const dbConnect = dbo.getDb();
+  const { user, name, html_url, avatar_url, numTotalTests, numPassedTests, numFailedTests } = req.body;
+
+  const document_ = {
+    user,
+    name,
+    html_url,
+    avatar_url,
+    numTotalTests,
+    numPassedTests,
+    numFailedTests
+  };
+
+  dbConnect
+    .collection('devs')
+    .insertOne(document_, (err, result) => {
+      if (err) {
+        res.status(400);
+      } else {
+        console.log(`Id Added: ${result.insertedId}`);
+        res.status(204);
       }
     });
 });
